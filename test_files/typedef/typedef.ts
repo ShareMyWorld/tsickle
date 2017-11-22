6e42f140d992698f344019358b553b41bc8c52f5
Martin Probst <martin@probst.io>
2017-11-22 12:40:41 +0100

    Extract the tests for generic typedefs into a separate file.

--- test_files/typedef/typedef.ts
+++ test_files/typedef/typedef.ts
@@ -15,13 +15,3 @@ type UnionTypeAliasUsingUnion = {x: UnionTypeAlias|boolean};
 function paramUsingUnion(x: UnionTypeAlias|boolean) {}
 
 export type ExportedType = string;
-
-interface TypeWithGenericArgAndDefault<T, U=UnionTypeAlias> {}
-type TypeAliasWithDefaultArgs = TypeWithGenericArgAndDefault<number>;
-
-interface TypeWithGenericArg<T> {}
-type TypeAliasWithTypeArgs<T> = TypeWithGenericArg<T>;
-
-// Repro to ensure we pass on the syntactical type node (TypeAliasWithTypeArgs)
-// through a two-level generic type argument.
-const varUsingArrayOfGenericAlias: TypeAliasWithTypeArgs<string>[] = [];
